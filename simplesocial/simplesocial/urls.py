d4fee1c08aa22aa19626a60ffe4814f6e6df9ee5
Mikaela Klein <mikaela.a.klein@gmail.com>
2021-01-18 17:48:30 -0800

    Add signup, login and logout page

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -20,4 +20,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
+    path('accounts/', include('accounts.urls'), name='accounts'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('test/', views.TestPage.as_view(), name='test'),
+    path('thanks/', views.ThanksPage.as_view(), name='thanks'),
 ]
